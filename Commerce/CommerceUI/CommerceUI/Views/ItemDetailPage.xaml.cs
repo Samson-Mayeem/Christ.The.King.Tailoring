d8e62a6f37dd20f4c8a26d9ca479fc88720e2773
= <=>
2022-12-22 13:59:27 +0000

    Add project files.

---
+++ Commerce/CommerceUI/CommerceUI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CommerceUI.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CommerceUI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
